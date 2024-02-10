88e4d3741305abb2d69754ea446c4c44473f99e6
maxim_home <berez_max@mail.ru>
2024-02-10 21:50:56 +0300

    regroup Дополнить описания

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col s1 m6">
-    <div class="card large hoverable orange accent-1">
+    <div class="card hoverable" :class="card_data.card_color">
       <div class="card-image waves-effect waves-block waves-light">
         <img
           class="activator"
@@ -9,18 +9,30 @@
         />
         <span class="card-title black-text">{{ card_data.title }}</span>
       </div>
-      <!-- <div class="card-content black-text">
+      <div class="card-content black-text">
         <p class="card-description left-align">{{ card_data.description }}</p>
-      </div> -->
-      <!-- <div class="card-action">
+      </div>
+      <div class="card-action">
         <a class="waves-effect waves-light activator btn-large">Состав</a>
-      </div> -->
-      <div class="card-reveal orange accent-1">
+      </div>
+      <div class="card-reveal" :class="card_data.card_color">
         <span class="card-title black-text">{{ card_data.title }}</span>
-        <p class="black-text">Состав: {{ card_data.composition }}</p>
+        <p class="black-text">Состав: {{ card_data.composition }}.</p>
       </div>
     </div>
   </div>
+
+  <!-- <div class="col s1 m6">
+    <div class="card">
+      <div class="card-image">
+        <img :src="'src/assets/' + card_data.picture">
+        <span class="card-title black-text">{{ card_data.title }}</span>
+      </div>
+      <div class="card-content">
+        <p class="card-description left-align">{{ card_data.description }}</p>
+      </div>
+    </div>
+  </div> -->
 </template>
 
 <script>
